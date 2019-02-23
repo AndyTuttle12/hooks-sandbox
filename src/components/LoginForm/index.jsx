16c990a41bc6cb10f4587dabcc3b2e6b01955681
Andy Tuttle <andytuttle12@gmail.com>
2019-02-22 22:35:29 -0500

    Adding more state and effects.

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
-const LoginForm = () => {
-  const [inputValue, setInputValue] = useState("");
+const isEnterKey = key => key === "Enter";
+
+const LoginForm = props => {
+  const [userValue, setUserValue] = useState("");
+  const [passValue, setPassValue] = useState("");
+  const [formSubmit, setFormSubmit] = useState(false);
+  useEffect(() => {
+    console.log(userValue);
+  }, [props]);
   return (
     <div className="login-form">
-      <label htmlFor="form-input">Input with Hooks!</label>
+      <label htmlFor="user-input">User Name:</label>
       <input
-        id="form-input"
+        id="user-input"
         className="input"
-        value={inputValue}
-        onChange={e => setInputValue(e.target.value)}
+        value={userValue}
+        onChange={e => setUserValue(e.target.value)}
+        onKeyPress={e => (isEnterKey(e.key) ? setFormSubmit(true) : "")}
       />
+      <label htmlFor="pass-input">Password:</label>
+      <input
+        type="password"
+        id="pass-input"
+        className="input"
+        value={passValue}
+        onChange={e => setPassValue(e.target.value)}
+        onKeyPress={e => (isEnterKey(e.key) ? setFormSubmit(true) : "")}
+      />
+      {`Submit form? ${formSubmit}`}
     </div>
   );
 };
